c9f9196371f9ea48bd5101fecceef297fc3fd972
shyhhh <hhhtheshy@gmail.com>
2023-04-03 23:14:11 +0800

    给 ProExperience 固定高

--- src/pages/ProExperience.tsx
+++ src/pages/ProExperience.tsx
@@ -8,15 +8,15 @@ interface IProps {
 
 export const ProExperience: React.FC<IProps> = (props) => {
   return (
-    <>
+    <div h-70 b-red b-1>
       <h1>项目经历</h1>
       <hr/>
       <Link to={'/home/demo1'}>橙子记账</Link>
       <Link to={'/home/demo2'}>Orange UI</Link>
       <Link to={'/home/demo3'}>可视化大屏</Link>
       <Suspense fallback=''>
-        <Outlet />
+        <Outlet/>
       </Suspense>
-    </>
+    </div>
   )
 }

c1d8a9a02507033cb30a76ab0dc6c2c2c06eeeb9
shyhhh <hhhtheshy@gmail.com>
2023-04-03 23:20:28 +0800

    recommend 添加 link 和 highlight

--- src/pages/Recommend.tsx
+++ src/pages/Recommend.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
+
 export const Recommend: React.FC = () => {
   return (
     <div>
       <h1>介绍</h1>
       <hr/>
-      我的 博客，目前阅读量 1.4W+，我的 Github
-      大三期间学校设有网页相关专业课，相关课程成绩优异均分 98，在校曾搭建过学院官网, 具有极客精神，积极关 注行业动态。
+      我的 <Link to="https://juejin.cn/user/3359726847928840/posts" text-orange-600>博客</Link>，目前阅读量 1.4W+，
+      我的 <Link to="https://github.com/shyhhh" text-orange-600>Github</Link> 大三期间学校设有网页相关专业课，相关课程成绩优异均分 98，在校曾搭建过学院官网, 具有极客精神，积极关注行业动态。
     </div>
   )
 }

3abfc2e2c579cbd41ffde9c4913152ea56f03976
shyhhh <hhhtheshy@gmail.com>
2023-04-04 09:59:42 +0800

    优化 nav 代码

--- src/pages/ProExperience.tsx
+++ src/pages/ProExperience.tsx
@@ -1,33 +1,27 @@
-import type { ReactNode } from 'react'
 import { Suspense } from 'react'
-import { Link, NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
-interface IProps {
-  children?: ReactNode
-}
-
-export const ProExperience: React.FC<IProps> = (props) => {
+export const ProExperience: React.FC = () => {
+  const navLinks = [
+    { to: '/home/demo1', text: '橙子记账' },
+    { to: '/home/demo2', text: 'Orange UI' },
+    { to: '/home/demo3', text: '可视化大屏' },
+  ]
   return (
     <div h-70>
       <h1>项目经历</h1>
       <hr />
-      <div children-p-x-30px children-p-y-10px children-b-1 m-b-20px children-m-x-5px children-rounded-sm>
-        <NavLink to={'/home/demo1'}
-          className={({ isActive }) =>
-            isActive ? 'active' : undefined}>
-          橙子记账
-        </NavLink>
-        <NavLink to={'/home/demo2'}
-          className={({ isActive }) =>
-            isActive ? 'active' : undefined}>
-          Orange UI
-        </NavLink>
-        <NavLink to={'/home/demo3'}
-          className={({ isActive }) =>
-            isActive ? 'active' : undefined}>
-          可视化大屏
-        </NavLink>
-      </div>
+      <nav children-p-x-30px children-p-y-10px children-b-1 m-b-20px children-m-x-5px children-rounded-sm>
+        {navLinks.map((link, index) => (
+          <NavLink
+            key={index}
+            to={link.to}
+            className={({ isActive }) => isActive ? 'active' : undefined}
+          >
+            {link.text}
+          </NavLink>
+        ))}
+      </nav>
       <Suspense fallback=''>
         <Outlet />
       </Suspense>
